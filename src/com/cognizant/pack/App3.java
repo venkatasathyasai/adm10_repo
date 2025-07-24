96e8790e2599858305b8212e623780df0b2f85a8
sathya <meriyalasathyasai@gmail.com>
2025-07-24 16:48:07 +0530

    Second commit

--- src/com/cognizant/pack/App3.java
+++ src/com/cognizant/pack/App3.java
@@ -1,9 +1,9 @@
 package com.cognizant.pack;
 
-public class App1 {
+public class App3 {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to app1");
+		System.out.println("Welcome to app3");
 
 	}
 
--- src/com/cognizant/pack/App4.java
+++ src/com/cognizant/pack/App4.java
@@ -1,9 +1,9 @@
 package com.cognizant.pack;
 
-public class App1 {
+public class App4 {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to app1");
+		System.out.println("Welcome to app4");
 
 	}
 
